90f40f7dea82b2a275271c98997246edd66f7fb1
김윤진(Kim YunJin) <yunjinkim25@mju.ac.kr>
2025-06-04 16:43:00 +0900

    week9

--- src/week9/HashingExperiment.java
+++ src/week9/HashingExperiment.java
@@ -1,37 +1,37 @@
 // 자료구조(6065) 과제 #8 (60241180 김윤진)
 package week9;
 
-import java.util.Arrays;
 import java.util.Random;
+import java.util.stream.IntStream;
 
 public class HashingExperiment {
     public static void main(String[] args) {
         // 0~99 사이 난수 20개 생성
-        Random rand = new Random();
-        Integer[] keys = new Integer[20];
-        for (int i = 0; i < 20; i++) {
-            keys[i] = rand.nextInt(100);
-        }
+        Integer[] keys = new Random().ints(20, 0, 100)
+                .boxed()
+                .toArray(Integer[]::new);
+
         System.out.println("자료구조(6065) 과제 #8 (60241180 김윤진)");
         System.out.println();
-        System.out.println("삽입할 키: " + Arrays.toString(keys));
+        System.out.print("삽입할 키: ");
+        IntStream.range(0, keys.length).forEach(i -> System.out.print(keys[i] + (i < keys.length - 1 ? ", " : "\n")));
 
         LinearProbing<Integer, String> lp = new LinearProbing<>();
         QuadProbing<Integer, String> qp = new QuadProbing<>();
         DoubleHashing<Integer, String> dh = new DoubleHashing<>();
         Chaining<Integer, String> ch = new Chaining<>();
 
-        // LinearProbing
+        // Linear Probing
         System.out.println("\nLinear Probing:");
         for (Integer key : keys) lp.insertWithProbeCount(key);
         printStats(lp.collisionCount, lp.totalProbeCount, lp.maxProbeCount);
 
-        // QuadProbing
+        // Quadratic Probing
         System.out.println("\nQuadratic Probing:");
         for (Integer key : keys) qp.insertWithProbeCount(key);
         printStats(qp.collisionCount, qp.totalProbeCount, qp.maxProbeCount);
 
-        // DoubleHashing
+        // Double Hashing
         System.out.println("\nDouble Hashing:");
         for (Integer key : keys) dh.insertWithProbeCount(key);
         printStats(dh.collisionCount, dh.totalProbeCount, dh.maxProbeCount);
